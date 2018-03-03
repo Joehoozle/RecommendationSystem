3c4d6cde0c3cc703a96671bb113ffc62ed3dcdb2
Unknown <dantedg@comcast.net>
2018-03-03 12:53:52 -0800

    0.82 -- Pearson works

--- functions.py
+++ functions.py
@@ -11,28 +11,33 @@ def cosineDiff(a,b):
     sim = tracker / ((sizeA ** 0.5) * (sizeB ** 0.5))
     return sim
 
+
+
 def diffCompare(a):
     if type(a) == tuple:
         return a[1]
     else:
         return a
 
-def pearsonCorrelation(a,b):
-    avgA = 0
-    avgB = 0
+
+
+
+def pearsonCorrWeight(a,b,avgA,avgB):
     tracker = 0
     sizeA = 0
     sizeB = 0
     if(len(a) != len(b)):
         return -2
     for i in range(0,len(a)):
-        avgA = avgA + a[i]
-        avgB = avgB + b[i]
-    avgA = avgA / len(a)
-    avgB = avgB / len(b)
-    for i in range(0,len(a)):
         tracker = tracker + ((a[i] - avgA) * (b[i] - avgB))
         sizeA = sizeA + ((a[i] - avgA) ** 2)
         sizeB = sizeB + ((b[i] - avgB) ** 2)
     sim = tracker / ((sizeA ** 0.5) * (sizeB ** 0.5))
     return sim
+
+
+def pearsonCompare(a):
+    if type(a) == tuple:
+        return abs(a[1])
+    else:
+        return a
--- pearsonTest.py
+++ pearsonTest.py
@@ -6,13 +6,13 @@ from functions import *
 
 #load all of my data
 data = np.loadtxt("train.txt", delimiter="\t")
-testData = np.loadtxt("test5.txt", dtype=int)
+testData = np.loadtxt("test20.txt", dtype=int)
 
 # offset depending on test size as user ids need to be normalized
-offset = 0
+offset = 200
 
 # this is how many neighbors I need
-k = 10
+k = 5
 
 # put new data into a list
 newRows = [[0 for x in range(1000)] for y in range(100)]
@@ -27,16 +27,34 @@ data = np.vstack((data,newRows))
 # test data/test cases in list format
 testRows = testData.tolist()
 
+# calculate average rating for all users
+averages = [0 for x in range(300)]
+for i in range(len(data)):
+    count = 0
+    avg = 0
+    for j in range(1000):
+        if data[i][j] != 0:
+            count = count + 1
+        avg = avg + data[i][j]
+    averages[i] = avg / count
+
+
+# save Pearson Correlation similarity
+textFile = open("averages.txt", "w")
+textFile.write(str(averages))
+textFile.close()
+
+
 # calculate cosine similarity of all pairs
-cosDiff = [[0 for x in range(len(data))] for y in range(len(data))]
+pearsonDiff = [[0 for x in range(len(data))] for y in range(len(data))]
 for i in range(len(data)):
     for j in range(200):
-        if i == j or cosDiff[i][j] != 0:
+        if i == j or pearsonDiff[i][j] != 0:
             continue
-        c = cosineDiff(data[i],data[j])
-        cosDiff[i][j] = (j,c)
-        cosDiff[j][i] = (i,c)
-    cosDiff[i].sort(key = diffCompare, reverse = True)
+        c = pearsonCorrWeight(data[i],data[j],averages[i],averages[j])
+        pearsonDiff[i][j] = (j,c)
+        pearsonDiff[j][i] = (i,c)
+    pearsonDiff[i].sort(key = pearsonCompare, reverse = True)
 
 # calculate average of k-nearest neighbors
 count1 = 0
@@ -50,16 +68,16 @@ for i in range(len(testRows)):
     weightN = 0
     weightD = 0
     for p in range(k):
-        neighbor = cosDiff[user][p][0]
-        neighborDifference = cosDiff[user][p][1]
+        neighbor = pearsonDiff[user][p][0]
+        neighborDifference = pearsonDiff[user][p][1]
         if(data[neighbor][movie] != 0):
-            weightN = weightN + (data[neighbor][movie] * neighborDifference)
-            weightD = weightD + (neighborDifference)
+            weightN = weightN + (neighborDifference * (data[neighbor][movie] - averages[neighbor]))
+            weightD = weightD + (abs(neighborDifference))
         else:
-            weightN = weightN + (3 * neighborDifference)
-            weightD = weightD + (neighborDifference)
+        #     weightN = weightN + (neighborDifference * (dataint(round(averages[neighbor]))))
+            weightD = weightD + (abs(neighborDifference))
 
-    prediction = weightN / weightD
+    prediction = averages[user] + (weightN / weightD)
     testRows[i][2] = int(round(prediction))
     if(testRows[i][2] == 3):
         count1 = count1 + 1
@@ -69,14 +87,8 @@ for i in range(len(testRows)):
 print str(count1)
 print str(count2)
 
-# save cosine similarity
-textFile = open("testCosDiff.txt", "w")
-for i in range(10):
-    textFile.write(str(cosDiff[i+200]) + "\n")
-textFile.close()
-
-# save edited
-textFile = open("cosineTest5.txt","w")
+# save result file
+textFile = open("pearsonTest20.txt","w")
 for i in range(len(output)):
     textFile.write(str(output[i][0]) + " " + str(output[i][1]) + " " + str(output[i][2]) + "\n")
 textFile.close()
